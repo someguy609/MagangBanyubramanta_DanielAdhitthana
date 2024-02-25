671a7e777e89fe34623f663cb30e6f9d3f4f7479
Daniel Adhitthana <dadhitthana@gmail.com>
2024-02-25 19:07:21 +0700

    feat: usb communicator
    add ros 2 communicator node with message encoding

--- src/communicator/src/node.cpp
+++ src/communicator/src/node.cpp
@@ -5,26 +5,52 @@
 #include <asio.hpp>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "interfaces/msg/motion.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+#define PORT "/dev/ttyACM0"
+
 class Communicator : public rclcpp::Node
 {
 public:
-    Communicator() : Node("communicator")
+    Communicator() : Node("communicator"), port_(asio::serial_port(io_, PORT))
     {
-        timer_ = this->create_wall_timer(
-            500ms, std::bind(&Communicator::timer_callback, this));
+        port_.set_option(asio::serial_port_base::baud_rate(115200));
+        port_.set_option(asio::serial_port_base::flow_control(asio::serial_port_base::flow_control::software));
+        port_.set_option(asio::serial_port_base::parity(asio::serial_port_base::parity::none));
+        port_.set_option(asio::serial_port_base::stop_bits(asio::serial_port_base::stop_bits::one));
+        port_.set_option(asio::serial_port_base::character_size(8));
+
+        subscriber_ = this->create_subscription<interfaces::msg::Motion>("motion_command", 10, std::bind(&Communicator::topic_callback, this, _1));
     }
 
 private:
-    void timer_callback()
+    void encode(char *dest, int value)
     {
+        int neg = value < 0;
+
+        dest[0] = (value * (neg ? -1 : 1) >> 8) + neg;
+        dest[1] = value & 255;
     }
 
-    rclcpp::TimerBase::SharedPtr timer_;
+    void topic_callback(const interfaces::msg::Motion &message)
+    {
+        char buff[8];
+
+        encode(buff, message.x_cmd);
+        encode(buff + 2, message.y_cmd);
+        encode(buff + 4, message.yaw);
+        encode(buff + 6, message.depth);
+
+        RCLCPP_INFO(this->get_logger(), "Sending \"%s\"", buff);
+        asio::write(port_, asio::buffer(buff, sizeof(buff)));
+    }
+
+    rclcpp::Subscription<interfaces::msg::Motion>::SharedPtr subscriber_;
+    asio::io_service io_;
+    asio::serial_port port_;
 };
 
 int main(int argc, char *argv[])

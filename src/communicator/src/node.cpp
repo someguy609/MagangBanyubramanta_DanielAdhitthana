496dc6364bb5d2e6f4429e7f35b2b928ab2ceea2
Daniel Adhitthana <dadhitthana@gmail.com>
2024-02-26 14:06:49 +0700

    refactor: reduce negative indicator thresh

--- src/communicator/src/node.cpp
+++ src/communicator/src/node.cpp
@@ -32,7 +32,7 @@ private:
 	{
 		int neg = value < 0;
 		value *= neg ? -1 : 1;
-		dest[0] = (value >> 8) + neg * 3;
+		dest[0] = (value >> 8) + neg * 2;
 		dest[1] = value & 0xFF;
 	}
 
@@ -41,8 +41,8 @@ private:
 		int top = src[0];
 		int bottom = src[1];
 
-		int neg = top > 2;
-		top -= neg * 3;
+		int neg = top > 1;
+		top -= neg * 2;
 
 		int value = ((top & 0x7F) << 8) | bottom;
 		if (neg)

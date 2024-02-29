9af5a9ba0a5c3eac7cb0c94d718f8356363ba8a2
Daniel Adhitthana <dadhitthana@gmail.com>
2024-02-29 19:37:58 +0700

    fix project structure

--- ros2/install/interfaces/local/lib/python3.10/dist-packages/interfaces/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from interfaces.msg._motion import Motion  # noqa: F401
-from interfaces.msg._object import Object  # noqa: F401
--- ros2/install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "msg/Motion.idl;msg/Object.idl")
-set(interfaces_INTERFACE_FILES "msg/Motion.msg;msg/Object.msg")
